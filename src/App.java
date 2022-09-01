8ded4222bfaae8f9075765301e9f8f744f68c1a3
gass-ita <andreanassi231205@gmail.com>
2022-09-01 02:32:50 +0200

    fixed all problems

--- src/App.java
+++ src/App.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         Cube c = new Cube();
         System.out.println(c);
-        System.out.println("Rotating...");
-        c.algorithem("M M");
-        System.out.println(c);
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            c.algorithem(sc.nextLine());
+            System.out.println(c);
+        }
+        
 
     }
 }
